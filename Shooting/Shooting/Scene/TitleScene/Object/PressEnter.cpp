e896189c31ae425a4aad35f7c38f7f44bbad668d
rikita-fujizono <ricky1002fe@gmail.com>
2019-02-25 15:11:23 +0900

    [issue #5 add] Enterキーを押すとゲームシーンに遷移する処理の実装

---
+++ Shooting/Shooting/Scene/TitleScene/Object/PressEnter.cpp
@@ -0,0 +1,53 @@
+﻿#include "PressEnter.h"
+#include "../../../GameLib/GameLib.h"
+#include "../../GameScene/GameScene.h"
+
+
+PressEnter::PressEnter()
+{
+	Initialize();
+}
+
+PressEnter::~PressEnter()
+{
+	Release();
+}
+
+//初期化する
+bool PressEnter::Initialize()
+{
+	DEBUGFONT->Initialize(m_FontSize);
+
+	return true;
+}
+
+//解放する
+void PressEnter::Release()
+{
+	DEBUGFONT->Release();
+}
+
+//更新する
+void PressEnter::Update()
+{
+	//フレーム経過で色情報を変える
+	m_frameCount++;
+	if (m_frameCount > m_ChangeColorTime)
+	{
+		//カラーが一定値達したら、フレームカウントを0に戻す
+		(m_Color <= static_cast<DWORD>(0x02ffffff)) ? m_frameCount = 0 : m_frameCount;
+
+		m_Color -= static_cast<DWORD>(0x03000000);
+	}
+
+	if (DIRECT_INPUT->KeyboardIsReleased(DIK_RETURN))
+	{
+		SCENEMANAGER->ChangeScene(new GameScene);
+	}
+}
+
+//描画する
+void PressEnter::Render()
+{
+	DEBUGFONT->DrawText(m_Text_x, m_Text_y, m_Color, m_PressEnter);
+}
---
+++ Shooting/Shooting/Scene/TitleScene/Object/PressEnter.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "../../../GameLib/ObjectManager/Object2DBase.h"
+#include "../../../GameLib/DirectX/DirectX.h"
+#include "../../../GameLib/2DHelper/2DHelper.h"
+#include "../../../GameLib/Texture/Texture.h"
+
+
+/**
+* @brief タイトルメニュー（PRESS　ENTER）の処理をまとめたクラス
+*/
+class PressEnter : public Object2DBase
+{
+public:
+
+	PressEnter();
+	~PressEnter();
+
+
+	/**
+	* @brief 初期化する関数
+	* @return 初期化が上手くできていないとFALSE
+	*/
+	bool Initialize();
+
+	/**
+	* @brief 解放する関数
+	*/
+	void Release();
+
+	/**
+	* @brief 操作関数
+	* @param タイトルメニューの何番のステップか
+	*/
+	void Update();
+
+	/**
+	* @brief 描画関数
+	*/
+	void Render();
+
+private:
+	int m_FontSize = 50;
+	DWORD m_Color = static_cast<DWORD>(0xffffffff);
+	int m_frameCount = 0;     //!< フレームを数えるための変数
+	int m_ChangeColorTime = 60;
+	const char* m_PressEnter = "PRESS ENTER";
+	int m_Text_x = 185;
+	int m_Text_y = 400;
+};
+
